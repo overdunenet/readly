ae72be0d5e19afe1428a7c2289ed184f2f44ed3b
Jun <qurugi0347@gmail.com>
2025-07-11 22:06:54 +0900

    CHORE: lint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,18 +1,13 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {SharedModule} from '@src/module/shared/shared.module';
-import {UserModule} from '@src/module/user/user.module';
-import {PostModule} from '@src/module/post/post.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SharedModule } from '@src/module/shared/shared.module';
+import { UserModule } from '@src/module/user/user.module';
+import { PostModule } from '@src/module/post/post.module';
 
 @Module({
-  imports: [
-    SharedModule,
-    UserModule,
-    PostModule
-  ],
+  imports: [SharedModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
